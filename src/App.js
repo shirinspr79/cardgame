9a876a5cb524ebe42b44f00327615e4b20f5e327
shirin shapoori <shrn@shirinspr.shrn>
2022-07-16 13:02:30 +0430

    NavBar Pills

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Route, Routes} from 'react-router-dom'
 import  Main  from './components/Main'
 import { Game } from './containers/Game';
-import { CustomNavbar } from './components/Navbar'
+import { About } from './containers/About';
+import { CustomNavbar } from './components/Navbar';
+
  
 export default function App(){
   return(
    <>
-      {/* <CustomNavbar /> */}
+      <CustomNavbar />
       <Routes> 
+        <Route path='/main' element={<Main />} />
         <Route path='/game' element={<Game />} />
-        <Route path='/' element={<Main />} />
+        <Route path='/about' element={<About />} />
       </Routes>
-    </>);
+  </>);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,68 @@
-import React from "react"
-import { Navigate } from 'react-router-dom'
+import React from "react";
+import { Navigate } from "react-router-dom";
+// import { Button } from 'react-bootstrap'
+import { Nav } from "react-bootstrap";
+import { useState } from "react";
 
-export const CustomNavbar = () => {
-    return (
+export function CustomNavbar() {
+  const [basicActive, setActive] = useState("main");
 
-        <nav>
-          <Navigate to='Main'>Home</Navigate>
-          <Navigate to='/'>Game</Navigate>
-          <Navigate to='about'>About me</Navigate>
-          {/* <Navigate>Github</Navigate> */}
-        </nav>
-     
-     )
+  const handleBasicClick = (value) => {
+    setActive(value);
+  };
+
+  return (
+    <>
+      <Nav variant="pills">
+        <Nav.Item
+          onClick={() => handleBasicClick("main")}
+          active={basicActive === "main"}
+        >
+          <Nav.Link to="/main" tag={Navigate}>
+            Home
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item
+          onClick={() => handleBasicClick("game")}
+          active={basicActive === "game"}
+        >
+          <Nav.Link to="/game" tag={Navigate}>
+            Game
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item
+          onClick={() => handleBasicClick("about")}
+          active={basicActive === "about"}
+        >
+          <Nav.Link to="/about" tag={Navigate}>
+            About Us
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
+  );
 }
+
+// <Navbar >
+//  <Nav variant="pills" defaultActiveKey="/main">
+//     <Nav.Item>
+//       <Nav.Link href="/main">Home</Nav.Link>
+//     </Nav.Item>
+//     <Nav.Item>
+//       <Nav.Link href="Game">Game</Nav.Link>
+//     </Nav.Item>
+//     <Nav.Item >
+//       <Nav.Link href="about">About me</Nav.Link>
+//     </Nav.Item>
+//   </Nav>
+//   </Navbar>
+
+// <nav>
+//  <button onClick={ ()=> navigate('main')}>Home</button>
+//  <button onClick={ ()=> navigate('game')}>Game</button>
+//  <button onClick={ ()=> navigate('about')}>About me</button>
+//   {/* <Navigate to='Main'>Home</Navigate>
+//   <Navigate to='/'>Game</Navigate>
+//   <Navigate to='/about'>About me</Navigate>
+//   <Navigate>Github</Navigate> */}
+// </nav>
